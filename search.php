75ec5e5d536289f36a9130889a1e2b2f05fb55fb
yuzucha01 <78517616+yuzucha01@users.noreply.github.com>
2021-03-24 22:25:13 +0900

    php一式

---
+++ search.php
@@ -0,0 +1,67 @@
+<?php
+session_start();
+
+include("funcs.php");
+loginCheck();
+
+require_once "./dbc.php";
+$searches = search();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RECIPE CHAT</title>
+    <link rel="stylesheet" href="css/chat.css">
+</head>
+<body>
+    <header class="leftNavigation">
+        <h1>RECIPE CHAT</h1>
+        <!-- 複数のものを送る -->
+        <form enctype="multipart/form-data" action="chat_upload.php" method="POST">
+            <div class="file-up">                <!-- valueは1MBのサイズの数字 -->
+                <input type="hidden" name="MAX_FILE_SIZE" value="1048576">
+                <!-- 画像だけ選択するaccept -->
+                <input class=file type="file" name="img" accept="image/*">
+            </div>
+            <div>
+                <textarea name="name" class="name" placeholder="料理名" cols="40" rows="2"></textarea>
+                <textarea name="zairyo" class="text" placeholder="材料" cols="40" rows="20"></textarea>
+                <textarea name="recipe" class="text" placeholder="レシピ" cols="40" rows="20"></textarea>
+            </div>
+            <div class="submit">
+                <input type="submit" value="送信" class="btn">
+            </div>
+        </form>
+        <button class="btn" type=“button” onclick="location.href='./top-page.php'">Homeに戻る</button>
+    </header>
+
+    <div class="content-main">
+
+         <form action="search.php" method="post">
+            <input type="text" name="search">
+            <input type="submit">
+            <button class="btn" type=“submit” onclick="location.href='./chat.php'">全体に戻る</button>
+        </form>
+
+        <div class="content">
+            <!-- asはイコール（として） -->
+            <?php foreach($searches as $search):?>
+                <div class="chat-contents">
+                    <img src="<?php echo "{$search['file_path']}"; ?>" alt="">
+                    <p><?php echo "{$search['name']}"; ?></p>
+                    <p><?php echo "{$search['zairyo']}"; ?></p>
+                    <p><?php echo "{$search['recipe']}"; ?></p>
+                    <p><?php echo "{$search['insert_time']}"; ?></p>
+                </div>
+            <?php endforeach; ?>
+         </div>
+
+
+    </div>
+
+</body>
+</html>
